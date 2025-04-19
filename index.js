6ceb57e547c17d868570aa8ce84f86abfd7f47e1
titi90213 <thibaut.23690@gmail.com>
2025-04-19 16:17:26 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+// index.js
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+module.exports = async (req, res) => {
+  if (req.method === 'GET') {
+    const prompt = req.query.prompt;
+
+    try {
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: prompt }],
+      });
+
+      const response = completion.data.choices[0].message.content;
+      res.status(200).json({ response: response });
+    } catch (error) {
+      res.status(500).json({ error: "Error contacting OpenAI API" });
+    }
+  } else {
+    res.status(404).send("Only GET method is supported.");
+  }
+};
